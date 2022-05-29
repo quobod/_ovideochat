96ce5ce73da024104820f27f8d410cbfcfe90d42
quobod <quobod@gmail.com>
2022-05-29 16:41:41 -0400

    Editing contact form

--- public/assets/js/singlecontactelements.js
+++ public/assets/js/singlecontactelements.js
@@ -1,8 +1,6 @@
 // Single contact controls
 export const contactEditButton = document.querySelector("#contact-edit-button");
-export const contactDeleteButton = document.querySelector(
-  "#contact-delete-button"
-);
+export const deleteContactLink = document.querySelector("#delete-contact");
 export const editContactForm = document.querySelector("#edit-contact-form");
 export const addEmailButton = document.querySelector("#add-email");
 export const addPhoneButton = document.querySelector("#add-phone");
--- public/assets/js/singlecontactmain.js
+++ public/assets/js/singlecontactmain.js
@@ -9,48 +9,7 @@ import {
   appendBeforeLastChild,
   addClickHandler,
 } from "./utils.js";
-/* 
-addHandler(elements.addEmailButton, "click", () => {
-  log(`\n\tAdded email field\n`);
-  const editContactForm = elements.editContactForm;
 
-  const divInputGroup = newElement("div");
-  const formGroupLabelSpan = newElement("span");
-  const formGroupLabelIcon = newElement("i");
-  const emailInput = newElement("input");
-  const trashIcon = newElement("i");
-
-  // Add attributes
-  addAttribute(divInputGroup, "class", "form-group");
-  addAttribute(formGroupLabelSpan, "class", "form-group-label");
-  addAttribute(formGroupLabelIcon, "class", "bi bi-envelope");
-  addAttribute(emailInput, "type", "email");
-  addAttribute(emailInput, "required", "");
-  addAttribute(emailInput, "class", "form-control");
-  addAttribute(
-    emailInput,
-    "name",
-    `email${editContactForm.children.length + 1}`
-  );
-  addAttribute(trashIcon, "class", "bi bi-trash");
-  // addAttribute(trashIcon, "style", "margin-left:5px;margin-top:10px; ");
-
-  // Config elements
-  // spanInputGroupLabel.innerHTML = `<i class="fa-solid fa-envelope"></i>`;
-
-  // Append to document
-  appendBeforeLastChild(editContactForm, divInputGroup);
-  appendChild(divInputGroup, formGroupLabelSpan);
-  appendChild(divInputGroup, emailInput);
-  appendChild(divInputGroup, trashIcon);
-  appendChild(formGroupLabelSpan, formGroupLabelIcon);
-
-  // Handle any events
-  addHandler(trashIcon, "click", () => {
-    divInputGroup.remove();
-  });
-});
- */
 addHandler(elements.addEmailButton, "click", () => {
   log(`\n\tAdded phone field\n`);
   const editContactForm = elements.editContactForm;
@@ -130,4 +89,8 @@ addHandler(elements.addPhoneButton, "click", () => {
   addHandler(trashIcon, "click", () => {
     divInputGroup.remove();
   });
+});
+
+addClickHandler(document.querySelector("#delete-this-document"), () => {
+  log("deleted!");
 });
